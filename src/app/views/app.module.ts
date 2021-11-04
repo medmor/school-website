62302052f035b9191e9c372448a9609d55e64201
medmor <elmadioune@gmail.com>
2021-11-04 22:12:07 +0100

    docs

--- src/app/views/app.module.ts
+++ src/app/views/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarModule } from './navbar/navbar.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,5 +11,6 @@ import { HttpClientModule } from '@angular/common/http';
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, NavbarModule, HttpClientModule],
     bootstrap: [AppComponent],
+    providers: [{ provide: APP_BASE_HREF, useValue: 'https://medmor.github.io/school-website/' }],
 })
 export class AppModule {}
